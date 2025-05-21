ed4756218ce8e0a537204d7bf3dca5c61ada2a33
Clarice <clariceting2006@gmail.com>
2025-05-21 17:27:12 +0700

    HTML for nav & footer

---
+++ beranda.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>UC Collab | Beranda</title>
+</head>
+<body>
+    <!-- Navigation Bar -->
+    <nav>
+        <a><img src="" alt=""></a>
+        <ul>
+            <li><a>Beranda</a></li>
+            <li><a>Kepanitiaan</a></li>
+            <li><a>Daftar</a></li>
+            <li><a>Masuk</a></li>
+        </ul>
+    </nav>
+
+    <!-- Header -->
+    <header>
+        <img src="" alt="">
+        <h1>UC Collab</h1>
+        <p>"Jembatan Kolaborasi Acara dan Panitia di Universitas Ciputra."</p>
+        <button>Telusuri Kepanitiaan</button>
+    </header>
+
+    <!-- Footer -->
+    <footer>
+        <div>
+        <!-- Footer Logo -->
+            <div>
+                <img src="" alt="">
+            </div>
+
+            <!-- Kontak Kami -->
+            <div>
+                <ul>
+                    <!-- Email -->
+                    <li>
+                        <img src="" alt="">
+                        <p><a>clarstasia@gmail.com</a></p>
+                    </li>
+                    <!-- Instagram -->
+                    <li>
+                        <img src="" alt="">
+                        <p><a>@clarstasia</a></p>
+                    </li>
+                    <!-- YouTube -->
+                    <li>
+                        <img src="" alt="">
+                        <p><a>Demo Video Link</a></p>
+                    </li>
+                </ul>
+            </div>
+
+            <!-- Tautan -->
+            <div>
+                <ul>
+                    <li><a>Beranda</a></li>
+                    <li><a>Kepanitiaan</a></li>
+                    <li><a>Daftar</a></li>
+                    <li><a>Masuk</a></li>
+                </ul> 
+            </div>
+
+            <p id="copyright" class="">&#169; 2025 Anastasia Eugene Maylinda and Clarice Harijanto. All rights reserved</p>
+        </div>
+    </footer>
+</body>
+</html>
